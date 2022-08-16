fb3fef10bd7f0caa51ece1e01cf1aefa49680e50
Michael Widowitz <michael@widowitz.com>
2022-08-16 17:56:24 +0300

    added a second strategy - with a larger sample

--- halting.py
+++ halting.py
@@ -27,13 +27,23 @@ def make_selection(numbers, strategy = 1):
     return value:
         an integer value, the ultimately selected number
     """
-    assert strategy in [1], "Strategy not implemented" 
+    assert strategy in [1, 2], "Strategy not implemented" 
     length = len(numbers)
     assert length > 1, "Need to have at least two cards for this game to make sense"
     if (strategy == 1):
         if (length == 2): # just stop at the very first card, it's 50:50 anyway
             return(numbers[0])
         sample_size = round(length / math.e)
+        training_sample = numbers[:sample_size]
+        remainder = numbers[sample_size:]
+        threshold = max(training_sample)
+        filtered_list = list(filter(lambda x: x > threshold, remainder))
+        selection = filtered_list[0] if filtered_list else remainder[-1] 
+        return(selection)
+    if (strategy == 2):
+        if (length == 2): # just stop at the very first card, it's 50:50 anyway
+            return(numbers[0])
+        sample_size = round(length / 2)
         training_sample = numbers[:sample_size]
         remainder = numbers[sample_size:]
         threshold = max(training_sample)
@@ -47,15 +57,15 @@ if __name__ == "__main__":
     NUMBER_OF_CARDS = 100
     MAXIMUM_VALUE = 100000
     MINIMUM_VALUE = 1
-    ITERATIONS = 10000
-    META_ITERATIONS = 5
+    ITERATIONS = 100000
+    META_ITERATIONS = 500
     for _ in range(META_ITERATIONS):
         attempts = 0
         successes = 0
         for _ in range(ITERATIONS):
             attempts = attempts + 1
             cards = generate_random_cards(NUMBER_OF_CARDS, MINIMUM_VALUE, MAXIMUM_VALUE)
-            selected = make_selection(cards)
+            selected = make_selection(cards, 2)
             optimum = max(cards)
             if (selected == optimum):
                 successes = successes + 1
