919de30deb025ae370109e4001aff0dea9ff1fa9
Michael Widowitz <michael@widowitz.com>
2022-08-16 18:16:51 +0300

    changed the main function to search for optimal sample size

--- halting.py
+++ halting.py
@@ -53,21 +53,54 @@ def make_selection(numbers, strategy = 1):
     return # should never be reached
 
 
+def make_selection_based_on_sampling(numbers, sample_size):
+    """
+    makes a selection by sampling,
+    i.e. look at the first <sample_size> numbers,
+    then pick the next larger one
+
+    params:
+        numbers: list of integers
+        sample_size: how many of these will be looked at before making decision
+
+    return value:
+        an integer value, the ultimately selected number
+    """
+    length = len(numbers)
+    assert length > 1, "Need to have at least two cards for this game to make sense"
+    assert sample_size < length, "Training sample must be smaller than number of cards"
+    if (length == 2): # just stop at the very first card, it's 50:50 anyway
+        return(numbers[0])
+    training_sample = numbers[:sample_size]
+    remainder = numbers[sample_size:]
+    threshold = max(training_sample)
+    filtered_list = list(filter(lambda x: x > threshold, remainder))
+    selection = filtered_list[0] if filtered_list else remainder[-1] 
+    return(selection)
+    
+
 if __name__ == "__main__":
     NUMBER_OF_CARDS = 100
     MAXIMUM_VALUE = 100000
     MINIMUM_VALUE = 1
-    ITERATIONS = 100000
-    META_ITERATIONS = 500
-    for _ in range(META_ITERATIONS):
+    ITERATIONS = 5000
+    optimum_sample_size = None
+    optimum_result_achieved = 0
+    for sample_size in range(1, NUMBER_OF_CARDS):
         attempts = 0
         successes = 0
+        print(f"Sample size: {sample_size}:")
         for _ in range(ITERATIONS):
             attempts = attempts + 1
             cards = generate_random_cards(NUMBER_OF_CARDS, MINIMUM_VALUE, MAXIMUM_VALUE)
-            selected = make_selection(cards, 2)
+            selected = make_selection_based_on_sampling(cards, sample_size)
             optimum = max(cards)
             if (selected == optimum):
                 successes = successes + 1
-        print(f'Success rate {successes / attempts:.3f} ({attempts} attempts, {successes} successful)')
+        success_rate = successes / attempts
+        print(f' Success rate {success_rate:.3f} ({attempts} attempts, {successes} successful)')
+        if (success_rate > optimum_result_achieved):
+            optimum_result_achieved = success_rate
+            optimum_sample_size = sample_size
+    print(f"Optimum sample size: {optimum_sample_size}, achieved success rate: {optimum_result_achieved:.3f}")
 
